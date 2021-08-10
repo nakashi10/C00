fbab5728edfdfc61f3484e150d8d9697cfbe8505
yunakash <nakashi1000@gmail.com>
2021-08-10 23:39:51 +0900

    a

--- ex06/ft_print_comb2.c
+++ ex06/ft_print_comb2.c
@@ -1,13 +1,12 @@
 #include <unistd.h>
-void	put_char1(int g_1);
-void	put_char3(int g_3);
-void	put_char(int g_n);
+void	put_char(int g_4, int g_3, int g_2, int g_1);
 int		g_n = 0;
 int		g_4 = 0;
 int		g_3 = 0;
 int		g_2 = 0;
-int 	g_1 = 1;
-void	ft_print_comb2()
+int		g_1 = 1;
+
+void	ft_print_comb2(void)
 {
 	while (g_4 < 10)
 	{
@@ -17,10 +16,7 @@ void	ft_print_comb2()
 			{
 				while (g_1 < 10)
 				{
-					put_char(g_4);
-					put_char3(g_3);
-					put_char(g_2);
-					put_char1(g_1);
+					put_char(g_4, g_3, g_2, g_1);
 					g_1++;
 				}
 				g_1 = 0;
@@ -37,29 +33,28 @@ void	ft_print_comb2()
 	}
 }
 
-void	put_char(int g_n)
+void	put_char(int g_4, int g_3, int g_2, int g_1)
 {
-	char	c;
+	char	c4;
+	char	c3;
+	char	c2;
+	char	c1;
 
-	c = '0' + g_n;
-	write(1, &c, 1);
-}
-
-void	put_char3(int g_3)
-{
-	char	c;
-
-	c = '0' + g_3;
-	write(1, &c, 1);
+	c4 = '0' + g_4;
+	c3 = '0' + g_3;
+	c2 = '0' + g_2;
+	c1 = '0' + g_1;
+	write(1, &c4, 1);
+	write(1, &c3, 1);
 	write(1, " ", 1);
+	write(1, &c2, 1);
+	write(1, &c1, 1);
+	if (!(g_4 == 9 && g_3 == 8))
+		write(1, ", ", 2);
 }
 
-void	put_char1(int g_1)
+int main()
 {
-	char	c;
-
-	c = '0' + g_1;
-	write(1, &c, 1);
-	if (! (g_4 == 9 && g_3 == 8))
-		write(1, ", ", 2);
+	ft_print_comb2();
+	return 0;
 }
