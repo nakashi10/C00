2b06688fc574ff11d7a24772861205fff418a7ae
yunakash <nakashi1000@gmail.com>
2021-08-10 23:16:06 +0900

    a

--- ex05/ft_print_comb.c
+++ ex05/ft_print_comb.c
@@ -1,23 +1,24 @@
 #include <unistd.h>
-void put_char1(int g_1,int g_3);
-void put_char(int n);
-int n=0;
-int g_3=0;
-int g_2=1;
-int g_1=2;
-void	ft_print_comb()
+void	put_char1(int g_1, int g_3);
+void	put_char(int n);
+int		n =0;
+int		g_3 =0;
+int		g_2 =1;
+int		g_1 =2;
+
+void	ft_print_comb(void)
 {
 	while (g_3 <= 7)
 	{
-		g_2 = g_3+1;
-		while(g_2 <= 8)
+		g_2 = g_3 + 1;
+		while (g_2 <= 8)
 		{
-			g_1 = g_2+1;
-			while(g_1 <= 9)
+			g_1 = g_2 + 1;
+			while (g_1 <= 9)
 			{
 				put_char(g_3);
 				put_char(g_2);
-				put_char1(g_1,g_3);
+				put_char1(g_1, g_3);
 				g_1++;
 			}
 			g_2++;
@@ -25,15 +26,21 @@ void	ft_print_comb()
 		g_3++;
 	}
 }
+
 void	put_char(int n)
 {
-	char c = '0' + n;
+	char	c;
+
+	c = '0' + n;
 	write(1, &c, 1);
 }
-void	put_char1(int g_1,int g_3)
+
+void	put_char1(int g_1, int g_3)
 {
-	char c = '0' + g_1;
+	char	c;
+
+	c = '0' + g_1;
 	write(1, &c, 1);
-	if(g_3<7)
+	if (g_3 < 7)
 		write(1, ", ", 2);
 }
